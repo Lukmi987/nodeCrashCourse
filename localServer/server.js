a356cd9882a523fa4e9aa6680feb2ed0d7bbfd98
lukas.komprs <lukas.komprs@studentagency.cz>
2021-02-15 09:29:26 +0100

    Request & Responses

---
+++ localServer/server.js
@@ -0,0 +1,61 @@
+const http = require('http');
+const fs = require('fs');
+
+//second arg is response header
+const server = http.createServer((req, res) => {
+    //when we make a request this callback fires
+    console.log('request made',req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-Type','text/html');
+
+    // set the static routing
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html'
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            //redirect
+            res.setHeader('Location','/about');
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    //send a html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            //res.write(data);
+            res.end(data);
+        }
+    });
+    // res.write('<p>hello ninjas</p>');
+    // res.write('<p>hello ninjas</p>');
+    // res.end();
+});
+
+//invoke listen mehtod
+//Port number are like 'doors' into a computer
+server.listen(3000,'localhost', () => {
+    console.log('listening for requests on port 3000')
+});
+
+//////////////////////////////////// NOTES /////////////////////////////////////
+// Status Codes
+// -status codes describe the type of response sent to the browser
+//     - 200 ok
+//     - 301- Resource moved
+//     - 404 - not found
+//     -500 Range - server error codes
